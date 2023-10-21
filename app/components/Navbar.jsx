5b0bf920d34e59446100ebb095d32b5ba747c4df
Brian Njuguna <brian@ibuqacapital.com>
2023-10-21 14:21:58 +0300

    add about me section

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import Link from "next/link";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/20/solid";
-import ManuOverlay  from "./MenuOverlay"
 import MenuOverlay from "./MenuOverlay";
 
 const NavLinks = [
