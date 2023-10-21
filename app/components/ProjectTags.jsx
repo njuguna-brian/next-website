1b4c3e0e3f3ef11f39e2b009619f20c89d845372
Brian Njuguna <brian@ibuqacapital.com>
2023-10-21 16:02:49 +0300

    add project section

---
+++ app/components/ProjectTags.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ProjectTags = ({ name, onClick, isSelected }) => {
+  const buttonStyles = isSelected
+    ? "text-white border-purple-500"
+    : "text-[#ADB7BE] boarder-slate-600 hover:boarder-whit";
+  return (
+    <button
+      className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
+      onClick={() => onClick(name)}
+    >
+      {name}
+    </button>
+  );
+};
+
+export default ProjectTags;
